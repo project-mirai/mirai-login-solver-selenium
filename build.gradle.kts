cdae786518fb3e0708b77efd9820144e801da80f
Karlatemp <karlatemp@vip.qq.com>
2021-01-10 00:33:33 +0800

    1.0-dev-14

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "net.mamoe"
-version = "1.0-dev-13"
+version = "1.0-dev-14"
 
 repositories {
     mavenCentral()
@@ -37,7 +37,7 @@ dependencies {
     }
 
     fun mxlib(module:String):String {
-        return "io.github.karlatemp.mxlib:mxlib-$module:3.0-dev-7"
+        return "io.github.karlatemp.mxlib:mxlib-$module:3.0-dev-8"
     }
 
     compileAndRuntime(kotlin("stdlib"))
